9963f87ab31fad9e55aaaeb69e82bee01232ebac
WillemRB <wrb@outlook.com>
2015-08-04 16:34:32 +0200

    Fix `t usage and build first backup version
    Switched all ' usage to " because otherwise the `t character doesn't appear as a \t.

--- backup.ps1
+++ backup.ps1
@@ -3,9 +3,10 @@
 
 function Log([System.String]$file, [System.String]$status, [System.ConsoleColor]$color) 
 {
-    Write-Host '`t[' -NoNewline
-    Write-Host $status -ForegroundColor $color
-    Write-Host '] $file'
+    Write-Host "`t[" -NoNewline
+    Write-Host $status -ForegroundColor $color -NoNewline
+    Write-Host "] " -NoNewline
+    Write-Host $file
 }
 
 function CopyIfNewer([System.String]$fileName, [System.String]$sourceDir, [System.String]$targetDir) 
@@ -15,13 +16,13 @@ function CopyIfNewer([System.String]$fileName, [System.String]$sourceDir, [Syste
 
     if (-not (Test-Path $sourcePath)) 
     {
-        Log $fileName 'REMOVE' Red
+        Log $fileName "REMOVE" Red
         return
     }
 
     if (-not (Test-Path $targetPath)) 
     {
-        Log $fileName 'CREATE' Green
+        Log $fileName "CREATE" Green
         Copy-Item $sourcePath $targetPath
         return
     }
@@ -31,23 +32,30 @@ function CopyIfNewer([System.String]$fileName, [System.String]$sourceDir, [Syste
 
     if ($sourceFile.LastWriteTime -gt $targetFile.LastWriteTime) 
     {
-        Log $fileName 'UPDATE' Green
+        Log $fileName "UPDATE" Green
         Copy-Item $sourcePath $targetPath
         return
     }
     elseif ($sourceFile.LastWriteTime -eq $targetFile.LastWriteTime) 
     {
-        Log $fileName ' SKIP ' Gray
+        Log $fileName " SKIP " Gray
         return
     }
     elseif ($sourceFile.LastWriteTime -lt $targetFile.LastWriteTime) 
     {
-        Log $fileName ' WARN ' Yellow
-        Write-Host '`t`tTarget is newer than Source'
+        Log $fileName " WARN " Yellow
+        Write-Host "`t`tTarget is newer than Source"
         return
     }
 }
 
 $games =  (Get-Content .\data.json | Out-String | ConvertFrom-Json)
 
-$games.games | % { Write-Host $_.Name }
+foreach ($game in $games.games)
+{
+    Write-Host $game.name
+    foreach ($file in $game.files)
+    {
+        CopyIfNewer $file $game.baseDir $game.targetDir
+    }
+}
