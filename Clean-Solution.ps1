1dc40fe2e6d6d6328991f81ba52b11250853e383
WillemRB <WillemRB@users.noreply.github.com>
2016-01-11 10:04:41 +0100

    Create Clean-Solution.ps1

---
+++ Clean-Solution.ps1
@@ -0,0 +1,22 @@
+# Script to clean Visual Studio solution folders.
+Param(
+    [Parameter()][string]$projectPath = ".",
+    [Parameter()][bool]$ignorePackages = $false
+)
+
+Get-ChildItem $ProjectPath -Recurse `
+| Where { $_.Name.ToLower() -in ("bin", "obj", "testresults", "packages") } `
+| ForEach `
+    {
+        $fullName = $_.FullName
+
+        if ($_.FullName.Contains("packages") -and $ignorePackages)
+        {
+            Write-Host "Skipped $fullName" -ForegroundColor Cyan
+        }
+        else
+        {
+            #Remove-Item $fullName -Recurse -Force
+            Write-Host "Removed $fullName" -ForegroundColor Red
+        }
+    }
