2869468d07b50bef17c1d8aeba05c322d80fbd93
WillemRB <WillemRB@users.noreply.github.com>
2016-01-12 09:56:01 +0100

    Handle directories

--- Clean-Solution.ps1
+++ Clean-Solution.ps1
@@ -6,21 +6,42 @@ Param(
     [Parameter()][bool]$ignorePackages = $false
 )
 
+Clear-Host
+
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
+function RemoveDirIfEmpty($directory)
+{
+    if (($directory.GetFiles().Length -eq 0) -and ($directory.GetDirectories().Length -eq 0) )
+    {
+        Remove-Item $directory -Recurse -Force
+        $name = $directory.BaseName
+        Write-Host "Remove directory $name" -ForegroundColor Green
+    }
+}
+
 Get-ChildItem $projectPath -Recurse `
-| Where { $_.FullName.ToLower() -match "(\\bin\\|\\obj\\|\\packages\\|\\testresults\\)" } `
+| Where { $_.FullName.ToLower() -match "(\\bin\\|\\obj\\|\\packages\\|\\testresults\\)(?!packages\.config$)" } `
 | ForEach `
     {
         $fullName = $_.FullName.Replace($scriptPath, "")
 
-        if ($_.FullName.Contains("packages") -and $ignorePackages)
+        if ($_.GetType().Name -eq "DirectoryInfo")
         {
+            RemoveDirIfEmpty $_
+        }
+        elseif ($_.FullName.Contains("packages") -and $ignorePackages)
+        {
+            # If packages are not to be removed
             Write-Host "Skipped $fullName" -ForegroundColor Cyan
         }
         else
         {
-            Remove-Item $fullName -Recurse -Force
+            $directory = $_.Directory
+
+            Remove-Item $_.FullName -Force
             Write-Host "Removed $fullName" -ForegroundColor Red
+
+            RemoveDirIfEmpty $directory
         }
     }
