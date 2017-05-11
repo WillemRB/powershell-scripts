fb73cd99b3b54834e1de1a877301036dee3d6660
WillemRB <WillemRB@users.noreply.github.com>
2017-05-11 08:27:47 +0200

    Use PowerShell cmdlets instead of aliases
    Replace the cmd aliases with PowerShell cmdlets in profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,15 +1,15 @@
 # Set environment variables for Visual Studio 2015 Command Prompt
 function VsCmd
 {
-    pushd "$env:VS140COMNTOOLS"
+    Push-Location "$env:VS140COMNTOOLS"
     cmd /c "vsvars32.bat&set" |
-        ForEach {
+        ForEach-Object {
             if ($_ -match "=") {
-                $v = $_.split("="); 
+                $v = $_.split("=");
                 Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
             }
         }
-    popd
+    Pop-Location
     
     Write-Host "Visual Studio 2015 Command Prompt variables set." -ForegroundColor Green
 }
